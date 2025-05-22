7a1053065454d4665d50115a9150b2472e8e0a8a
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-05-22 15:53:24 +0500

    Added Reducer, Add Todo functionality added

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -7,4 +7,13 @@ const initialState = {
 export const todoSlice = createSlice({
   name: "todo",
   initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+  },
 });
