1e8d0b6b9f7869eb326057175be21a4ff8fb7088
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-05-28 14:24:39 +0500

    Assign Variables to useSelector & useDispatch

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { removeTodo } from "../features/todo/todoSlice";
 
 const Todos = () => {
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
   return (
      <>
     <ul className="list-none">
