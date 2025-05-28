c3814d5a3cdd7631f19c607e193ff623d001b512
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-05-28 14:21:38 +0500

    Add Input State & Assign Variable To Use DIspatch

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,10 +1,12 @@
-import React ,{useState} from "react";
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addTodo } from "../features/todo/todoSlice";
 
 const AddTodo = () => {
-    const [input, setInput] = useState('')
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
+
+
   return (
     <div>
       <form className="space-x-3 mt-120" onSubmit={addTodoHandler}>
